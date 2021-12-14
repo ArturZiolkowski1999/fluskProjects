7e31391074581fa154b95754be9c6ddcb05e3070
Artur Ziółkowski <aaartiii99@gmail.com>
2021-12-15 00:56:03 +0100

    finish login form

--- Blog/app/auth/views.py
+++ Blog/app/auth/views.py
@@ -1,11 +1,20 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request, flash
 from . import auth
 from .forms import LoginForm
+from ..models.models import User
+from flask_login import login_user
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect(url_for('auth.login'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next = request.args.get('next')
+            if next is None or not next.startswth('/'):
+                next = url_for('main.index')
+        return redirect(url_for(next))
+    flash('Incorrect password or user name')
     return render_template('auth/login.html', form=form)
